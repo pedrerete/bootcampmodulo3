e47ec66f676cb7d0b1cfbd0fc6c4bb307851e7bb
Pedro Esparza <p_mauricio@hotmail.com>
2022-05-26 17:54:02 -0500

    se separo el  appmodules en los 3 modulos diferentes

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ListadoComponent } from './listado/listado.component';
+import { HeroeComponent } from './heroe/heroe.component';
+
+@NgModule({
+    declarations: [
+        HeroeComponent,
+        ListadoComponent,
+    ],
+    exports: [
+        ListadoComponent,
+        HeroeComponent
+    ],
+    imports: [
+        CommonModule
+    ],
+    providers: [],
+})
+export class HeroesModule {
+
+}
